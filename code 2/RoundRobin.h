4ffdc93e069dfe6bbfef7d0421fb01cf65aaecfd
Afify21 <lolesea21@gmail.com>
2025-04-30 23:33:59 +0300

    .

---
+++ code 2/RoundRobin.h
@@ -0,0 +1,9 @@
+//okay what do we need?
+//el arrival time
+//allocation 34an el C m7tag memory 
+//picking up the next ready process from the circular list,give it a quantum
+//updating the remaining time using proccess.c sneding it back to the schedular
+// tb what to do? 
+//awl 7aga mfrod te7sal howa eny lma 7aga t arrive a3mlha enqueue using function insertatend
+//tany haga if i either finished el remaining time aw aw el quantum b zero el mfrod a3ml remove current
+//34an hwa quantums f ana 3ayz a rotate 3la el list  using change current to the next one 
